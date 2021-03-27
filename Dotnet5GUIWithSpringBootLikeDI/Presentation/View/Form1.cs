79fde4588fe3fb31a6d0dc3c54c80d778845aa43
Kazuhito Miura <kazuhito.sumpic@gmail.com>
2021-03-27 20:50:49 +0900

    implement: フォームのタイトルにサービスからとった値を表示するように。
    いまはDI出来ていないので、エラーで落ちる。

--- Dotnet5GUIWithSpringBootLikeDI/Presentation/View/Form1.cs
+++ Dotnet5GUIWithSpringBootLikeDI/Presentation/View/Form1.cs
@@ -1,21 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dotnet5GUIWithSpringBootLikeDI.Appliraciton.Sevice.Miura;
 
 namespace Dotnet5GUIWithSpringBootLikeDI
 {
     public partial class Form1 : Form
     {
+        private readonly MiuraService service;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var miura = service.Get();
+            Text = miura.ToString();
+        }
     }
 }
